5a63c485fe19f153df377e9696c5615286d0b2cb
s-parvizi <sohail.parvizi@gmail.com>
2020-10-18 22:51:20 +0330

    Update samples

--- src/jsreview/index09.js
+++ src/jsreview/index09.js
@@ -1,10 +1,21 @@
 // Arrow Functions
 
 // old javascript function style
-const  square1 = function (number) {
+const square1 = function (number) {
     return number * number;
 };
 
 // Arrow function style
-const  square2 = number => number * number;
+const square2 = number => number * number;
 console.log(square2(5));
+
+const jobs = [
+    {id: 1, isActive: true},
+    {id: 2, isActive: true},
+    {id: 3, isActive: false}
+];
+
+const activeJobs1 = jobs.filter(function (job) {
+    return job.isActive;
+});
+const activeJobs2 = jobs.filter(job => job.isActive);
