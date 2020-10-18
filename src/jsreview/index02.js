2df8cc9f1ce85ffb776f47e9d33ca76f4bdc9053
s-parvizi <sohail.parvizi@gmail.com>
2020-10-18 22:38:35 +0330

    Update samples

--- src/jsreview/index02.js
+++ src/jsreview/index02.js
@@ -8,5 +8,24 @@ const person = {
       y:2
     },
     walk: function () {
-    }
+        console.log(this);
+    },
+    talk(){}
 };
+
+person.walk();
+person.talk();
+person['name'] = 'John';
+
+const targetMember = 'name';
+person[targetMember.valueOf()] = 'Bob';
+
+const walk = person.walk();
+// this will result in undefiled, when we use method as standalone function, this refers to window
+// in restricted mode reference to window object is undefined
+// in JS every function is an object
+walk();
+
+const walk2 = person.walk.bind(person);
+// the bind method fix the problem here
+walk2();
---
+++ src/jsreview/index09.js
@@ -0,0 +1,10 @@
+// Arrow Functions
+
+// old javascript function style
+const  square1 = function (number) {
+    return number * number;
+};
+
+// Arrow function style
+const  square2 = number => number * number;
+console.log(square2(5));
